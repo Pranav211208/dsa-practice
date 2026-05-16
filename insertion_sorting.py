b342223da232e595c594bcae90f15fa0259a913c
Pranav21108 <pranavsudarshan@gmail.com>
2026-05-16 20:50:18 +0530

    insertion sorting

---
+++ insertion_sorting.py
@@ -0,0 +1,10 @@
+def insertion_sort(arr):
+    for i in range(1,len(arr)): 
+        for j in range(i,0,-1):
+            if arr[j]<arr[j-1]:
+                arr[j],arr[j-1]=arr[j-1],arr[j]
+                print(arr)
+
+
+#insertion_sort([12, 25, 64, 22])
+insertion_sort([5, 3, 1, 4, 2])
